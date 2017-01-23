b14658df0bb117da0fc156f651456022d4631edd
Paul Gadi <paulgadi@gmail.com>
2017-01-23 16:24:49 +0800

    added initial unity test files

---
+++ Assets/WebsocketManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+
+public class WebsocketManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+      var ws = new WebSocket("ws://seed.gomix.me");
+      // var ws = new WebSocket("ws://localhost:8080");
+
+        ws.OnOpen += OnOpenHandler;
+        ws.OnMessage += OnMessageHandler;
+      //  ws.OnClose += OnCloseHandler;
+      //  ws.OnError += OnErrorHandler;
+
+        ws.ConnectAsync();
+
+	}
+	
+    private void OnOpenHandler(object sender, System.EventArgs e) {
+        Debug.Log("Websocket connected!");
+    }
+
+    private void OnMessageHandler(object sender, MessageEventArgs e) {
+        Debug.Log("Websocket said: " + e.Data);
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
