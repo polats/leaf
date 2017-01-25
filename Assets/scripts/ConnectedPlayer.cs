6566e9347c16dab98ea360db39c2025c388986fb
Paul Gadi <paulgadi@gmail.com>
2017-01-25 21:04:14 +0800

    added mouselook and observable on camera rotation

---
+++ Assets/scripts/ConnectedPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class ConnectedPlayer : MonoBehaviour {
+
+    public GameObject PlayerCamera;
+
+    // Use this for initialization
+	void Start () {
+
+        PlayerCamera.transform.ObserveEveryValueChanged(x => x.rotation).Subscribe(
+            rotationVector =>
+            {
+                Debug.Log(JsonUtility.ToJson(rotationVector));
+            });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
