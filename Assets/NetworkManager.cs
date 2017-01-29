a600ab1028a3cfbe8d09cbf26cc876fa0c314351
Paul Gadi <paulgadi@gmail.com>
2017-01-29 13:39:57 +0800

    test files and project settings

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+// Sample Data Classes that could be stringified by JSONUtility
+public class User
+{
+    public string uid;
+    public string displayname;
+
+    public User(string u,string d)
+    {
+        uid = u;
+        displayname = d;
+    }
+}
+
+public class MatchJoinResponse
+{
+    public bool result;
+}
+
+public class NetworkManager : MonoBehaviour
+{
+    void Start()
+    {
+        ConnectToServer(new User("polats", "polatski"));
+    }
+
+    public void ConnectToServer(User user)
+    { 
+        Debug.Log("Connecting...");
+
+        Application.ExternalCall ("Logon", JsonUtility.ToJson (user));
+    }
+
+    public void OnMatchJoined(string jsonresponse)
+    {
+        MatchJoinResponse mjr = JsonUtility.FromJson<MatchJoinResponse> (jsonresponse);
+        if(mjr.result)
+        {
+            Debug.Log("Logon successful");
+        }
+        else
+        {
+            Debug.Log("Logon failed");
+        }
+    }
+
+    public void BroadcastQuit()
+    {
+        Debug.Log("Quitting!");
+
+        Application.ExternalCall ("QuitMatch");
+    }
+}
